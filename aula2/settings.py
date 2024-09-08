e0e453ef1bd6be649049e548440ac6dce57efe95
Carleandro Noleto <carleandro7@gmail.com>
2024-09-08 09:51:52 -0300

    correção

--- aula2/settings.py
+++ aula2/settings.py
@@ -118,7 +118,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
-STATIC_ROOT = os.path.join('static')
 
 
 # Default primary key field type
