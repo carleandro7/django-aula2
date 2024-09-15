c84934a2b2f82c6983a0f4a952a496d2ce5a33f0
Carleandro Noleto <carleandro7@gmail.com>
2024-09-15 10:38:42 -0300

    adicionado pedido e removido arquivos de deletes html

--- clientes/forms.py
+++ clientes/forms.py
@@ -1,7 +1,15 @@
 from django import forms
 from .models import Cliente
+from .models import Pedido
 
 class ClienteForm(forms.ModelForm):
     class Meta:
         model = Cliente
         fields = ['nome', 'cpf']
+    
+
+
+class PedidoForm(forms.ModelForm):
+    class Meta:
+        model = Pedido
+        fields = ['descricao', 'valor', 'cliente']
--- clientes/views.py
+++ clientes/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Cliente
+from .models import Pedido
 from .forms import ClienteForm
+from .forms import PedidoForm
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -40,3 +42,40 @@ def deletar_cliente(request, id):
         cliente.delete()
         return redirect('listar_clientes')  # Redireciona para a lista de clientes
     return render(request, 'clientes/deletar_cliente.html', {'cliente': cliente})
+
+
+@login_required
+def listar_pedidos(request):
+    pedidos = Pedido.objects.all()
+    return render(request, 'pedidos/lista_pedidos.html', {'pedidos': pedidos})
+
+@login_required
+def criar_pedido(request):
+    if request.method == "POST":
+        form = PedidoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_pedidos')
+    else:
+        form = PedidoForm()
+    return render(request, 'pedidos/criar_pedido.html', {'form': form})
+
+@login_required
+def atualizar_pedido(request, id):
+    pedido = get_object_or_404(Pedido, id=id)
+    if request.method == "POST":
+        form = PedidoForm(request.POST, instance=pedido)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_pedidos')
+    else:
+        form = PedidoForm(instance=pedido)
+    return render(request, 'pedidos/atualizar_pedido.html', {'form': form})
+
+@login_required
+def deletar_pedido(request, id):
+    pedido = get_object_or_404(Pedido, id=id)
+    if request.method == "POST":
+        pedido.delete()
+        return redirect('listar_pedidos')  # Redireciona para a lista de pedidos
+    return render(request, 'pedidos/deletar_pedido.html', {'pedido': pedido})
